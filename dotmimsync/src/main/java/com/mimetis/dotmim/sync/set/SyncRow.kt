9b4c74fb6e28ca4cdf7981b77e8e654e106c80f1
Aleksei Starchikov <vunder@bk.ru>
2022-01-31 21:25:16 +0100

    Fixed SyncRow.fromArray

--- dotmimsync/src/main/java/com/mimetis/dotmim/sync/set/SyncRow.kt
+++ dotmimsync/src/main/java/com/mimetis/dotmim/sync/set/SyncRow.kt
@@ -69,7 +69,12 @@ class SyncRow(@Transient val length: Int = 0) {
             throw Exception("row must contains State on position 0 and UpdateScopeId on position 1")
 
         System.arraycopy(row, 1, buffer, 0, length)
-        this.rowState = row[0] as DataRowState
+        val drs = row[0]
+        this.rowState = if (drs is DataRowState) {
+            drs
+        } else {
+            DataRowState.values().first { it == drs }
+        }
     }
 
     fun clear() {
