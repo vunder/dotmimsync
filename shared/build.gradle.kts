a58b115c19e73f3e3d4e6215e70e04b56c95d493
Aleksei Starchikov <vunder@bk.ru>
2024-12-01 12:04:00 +0100

    Removed app

---
+++ shared/build.gradle.kts
@@ -0,0 +1,64 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_1_8)
+                }
+            }
+        }
+    }
+    
+//    listOf(
+//        iosX64(),
+//        iosArm64(),
+//        iosSimulatorArm64()
+//    ).forEach {
+//        it.binaries.framework {
+//            baseName = "shared"
+//            isStatic = true
+//        }
+//    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.jetbrains.serialization.core)
+            implementation(libs.jetbrains.serialization.json)
+//            implementation(libs.jetbrains.serialization.core.jvm)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.uuid)
+            implementation(libs.androidx.sqlite.bundled)
+
+            implementation(libs.bundles.ktor)
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.mimetis.dotmimsync"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 26
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
