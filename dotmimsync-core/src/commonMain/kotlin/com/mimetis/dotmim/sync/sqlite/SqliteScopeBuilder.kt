5210725d0fcb9a345089a7bcebe5a6a332882dad
Aleksei Starchikov <vunder@bk.ru>
2025-04-03 06:35:49 +0200

    SqliteScopeBuild with SQLiteConnection

--- dotmimsync-core/src/commonMain/kotlin/com/mimetis/dotmim/sync/sqlite/SqliteScopeBuilder.kt
+++ dotmimsync-core/src/commonMain/kotlin/com/mimetis/dotmim/sync/sqlite/SqliteScopeBuilder.kt
@@ -1,22 +1,21 @@
 package com.mimetis.dotmim.sync.sqlite
 
-import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
+import androidx.sqlite.SQLiteConnection
+import androidx.sqlite.SQLiteStatement
+import androidx.sqlite.execSQL
 import com.mimetis.dotmim.sync.SyncVersion
 import com.mimetis.dotmim.sync.builders.DbScopeBuilder
 import com.mimetis.dotmim.sync.scopes.ScopeInfo
 import com.mimetis.dotmim.sync.set.SyncSet
 import com.mimetis.dotmim.sync.setup.SyncSetup
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import java.util.*
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
 class SqliteScopeBuilder(
     scopeInfoTableName: String,
-    private val database: SQLiteDatabase
+    private val database: SQLiteConnection
 ) : DbScopeBuilder(scopeInfoTableName) {
 
     private val json = Json {
@@ -25,11 +24,10 @@ class SqliteScopeBuilder(
     }
 
     override fun existsScopeInfoTable(): Boolean =
-        database.rawQuery(
-            "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='${scopeInfoTableName.unquoted()}'",
-            null
+        database.prepare(
+            "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='${scopeInfoTableName.unquoted()}'"
         ).use { cursor ->
-            cursor.moveToNext() && cursor.getInt(0) > 0
+            cursor.step() && cursor.getInt(0) > 0
         }
 
 
@@ -50,7 +48,7 @@ class SqliteScopeBuilder(
     }
 
     override fun getAllScopes(scopeName: String): MutableList<ScopeInfo> =
-        database.rawQuery(
+        database.prepare(
             """SELECT sync_scope_id
                             , sync_scope_name
                             , sync_scope_schema
@@ -61,63 +59,95 @@ class SqliteScopeBuilder(
                             , scope_last_sync_timestamp
                             , scope_last_sync_duration
                             FROM ${scopeInfoTableName.unquoted()}
-                            WHERE sync_scope_name = ?""",
-            arrayOf(scopeName)
+                            WHERE sync_scope_name = ?"""
         ).use { cursor ->
+            cursor.bindText(1, scopeName)
             ArrayList<ScopeInfo>().apply {
-                while (cursor.moveToNext()) {
+                while (cursor.step()) {
                     add(readScope(cursor, json))
                 }
             }
         }
 
     override fun existsScopeInfo(scopeId: Uuid): Boolean =
-        database.rawQuery(
-            "Select count(*) from ${scopeInfoTableName.unquoted()} where sync_scope_id=?",
-            arrayOf(scopeId.toString())
+        database.prepare(
+            "Select count(*) from ${scopeInfoTableName.unquoted()} where sync_scope_id=?"
         ).use { cursor ->
-            cursor.moveToNext() && cursor.getInt(0) > 0
+            cursor.bindText(1, scopeId.toString())
+            cursor.step() && cursor.getInt(0) > 0
         }
 
     override fun insertScope(scopeInfo: ScopeInfo): ScopeInfo {
-        database.execSQL(
-            "Insert into ${scopeInfoTableName.unquoted()} (sync_scope_name, sync_scope_schema, sync_scope_setup, sync_scope_version, scope_last_sync, scope_last_sync_duration, scope_last_server_sync_timestamp, scope_last_sync_timestamp, sync_scope_id) values(?,?,?,?,?,?,?,?,?)",
-            arrayOf(
-                scopeInfo.name,
-                if (scopeInfo.schema != null) json.encodeToString(scopeInfo.schema) else null,
-                if (scopeInfo.setup != null) json.encodeToString(scopeInfo.setup) else null,
-                scopeInfo.version,
-                scopeInfo.lastSync?.toString(),
-                scopeInfo.lastSyncDuration.toString(),
-                scopeInfo.lastServerSyncTimestamp?.toString(),
-                scopeInfo.lastSyncTimestamp?.toString(),
-                scopeInfo.id.toString()
-            )
-        )
+        database.prepare(
+            "Insert into ${scopeInfoTableName.unquoted()} (sync_scope_name, sync_scope_schema, sync_scope_setup, sync_scope_version, scope_last_sync, scope_last_sync_duration, scope_last_server_sync_timestamp, scope_last_sync_timestamp, sync_scope_id) values(?,?,?,?,?,?,?,?,?)"
+        ).use {
+            it.bindText(1, scopeInfo.name)
+            if (scopeInfo.schema != null)
+                it.bindText(2, json.encodeToString(scopeInfo.schema))
+            else
+                it.bindNull(2)
+            if (scopeInfo.setup != null)
+                it.bindText(3, json.encodeToString(scopeInfo.setup))
+            else
+                it.bindNull(3)
+            it.bindText(4, scopeInfo.version)
+            if (scopeInfo.lastSync != null)
+                it.bindLong(5, scopeInfo.lastSync!!)
+            else
+                it.bindNull(5)
+            it.bindLong(6, scopeInfo.lastSyncDuration)
+            if (scopeInfo.lastServerSyncTimestamp != null)
+                it.bindLong(7, scopeInfo.lastServerSyncTimestamp!!)
+            else
+                it.bindNull(7)
+            if (scopeInfo.lastSyncTimestamp != null)
+                it.bindLong(8, scopeInfo.lastSyncTimestamp!!)
+            else
+                it.bindNull(8)
+            it.bindText(9, scopeInfo.id.toString())
+
+            it.step()
+        }
         return getAllScopes(scopeInfo.name).first()
     }
 
     override fun updateScope(scopeInfo: ScopeInfo): ScopeInfo {
-        database.execSQL(
-            "Update ${scopeInfoTableName.unquoted()} set sync_scope_name=?, sync_scope_schema=?, sync_scope_setup=?, sync_scope_version=?, scope_last_sync=?, scope_last_server_sync_timestamp=?, scope_last_sync_timestamp=?, scope_last_sync_duration=? where sync_scope_id=?",
-            arrayOf(
-                scopeInfo.name,
-                if (scopeInfo.schema != null) json.encodeToString(scopeInfo.schema) else null,
-                if (scopeInfo.setup != null) json.encodeToString(scopeInfo.setup) else null,
-                scopeInfo.version,
-                scopeInfo.lastSync?.toString(),
-                scopeInfo.lastServerSyncTimestamp?.toString(),
-                scopeInfo.lastSyncTimestamp?.toString(),
-                scopeInfo.lastSyncDuration.toString(),
-                scopeInfo.id.toString()
-            )
-        )
+        database.prepare(
+            "Update ${scopeInfoTableName.unquoted()} set sync_scope_name=?, sync_scope_schema=?, sync_scope_setup=?, sync_scope_version=?, scope_last_sync=?, scope_last_server_sync_timestamp=?, scope_last_sync_timestamp=?, scope_last_sync_duration=? where sync_scope_id=?"
+        ).use {
+            it.bindText(1, scopeInfo.name)
+            if (scopeInfo.schema != null)
+                it.bindText(2, json.encodeToString(scopeInfo.schema))
+            else
+                it.bindNull(2)
+            if (scopeInfo.setup != null)
+                it.bindText(3, json.encodeToString(scopeInfo.setup))
+            else
+                it.bindNull(3)
+            it.bindText(4, scopeInfo.version)
+            if (scopeInfo.lastSync != null)
+                it.bindLong(5, scopeInfo.lastSync!!)
+            else
+                it.bindNull(5)
+            if (scopeInfo.lastServerSyncTimestamp != null)
+                it.bindLong(6, scopeInfo.lastServerSyncTimestamp!!)
+            else
+                it.bindNull(6)
+            if (scopeInfo.lastSyncTimestamp != null)
+                it.bindLong(7, scopeInfo.lastSyncTimestamp!!)
+            else
+                it.bindNull(7)
+            it.bindLong(8, scopeInfo.lastSyncDuration)
+            it.bindText(9, scopeInfo.id.toString())
+
+            it.step()
+        }
         return getAllScopes(scopeInfo.name).first()
     }
 
     override fun getLocalTimestamp(): Long =
-        database.rawQuery("Select " + SqliteTableBuilder.TimestampValue, null).use { cursor ->
-            if (cursor.moveToNext())
+        database.prepare("Select " + SqliteTableBuilder.TimestampValue).use { cursor ->
+            if (cursor.step())
                 cursor.getLong(0)
             else
                 0L
@@ -127,31 +157,39 @@ class SqliteScopeBuilder(
         database.execSQL("DROP Table ${scopeInfoTableName.unquoted()}")
     }
 
-    private fun readScope(cursor: Cursor, json: Json) =
-        ScopeInfo(
-            id = Uuid.parse(cursor.getString(cursor.getColumnIndex("sync_scope_id"))),
-            name = cursor.getString(cursor.getColumnIndex("sync_scope_name")),
-            schema = if (cursor.isNull(cursor.getColumnIndex("sync_scope_schema"))) null else json.decodeFromString<SyncSet>(
-                cursor.getString(cursor.getColumnIndex("sync_scope_schema"))
-            ),
-            setup = if (cursor.isNull(cursor.getColumnIndex("sync_scope_setup"))) null else json.decodeFromString<SyncSetup>(
-                cursor.getString(cursor.getColumnIndex("sync_scope_setup"))
-            ),
-            version = (if (cursor.isNull(cursor.getColumnIndex("sync_scope_version"))) null else cursor.getString(
-                cursor.getColumnIndex("sync_scope_version")
-            )) ?: SyncVersion.current,
-            lastSync = if (cursor.isNull(cursor.getColumnIndex("scope_last_sync"))) null else cursor.getLong(
-                cursor.getColumnIndex("scope_last_sync")
-            ),
-            lastServerSyncTimestamp = if (cursor.isNull(cursor.getColumnIndex("scope_last_server_sync_timestamp"))) null else cursor.getLong(
-                cursor.getColumnIndex("scope_last_server_sync_timestamp")
-            ),
-            lastSyncTimestamp = if (cursor.isNull(cursor.getColumnIndex("scope_last_sync_timestamp"))) null else cursor.getLong(
-                cursor.getColumnIndex("scope_last_sync_timestamp")
-            ),
-            lastSyncDuration = if (cursor.isNull(cursor.getColumnIndex("scope_last_sync_duration"))) 0 else cursor.getLong(
-                cursor.getColumnIndex("scope_last_sync_duration")
-            ),
+    private fun readScope(cursor: SQLiteStatement, json: Json): ScopeInfo {
+        val columns = cursor.getColumnNames()
+        return ScopeInfo(
+            id = Uuid.parse(cursor.getText(columns.indexOf("sync_scope_id"))),
+            name = cursor.getText(columns.indexOf("sync_scope_name")),
+            schema = if (cursor.isNull(columns.indexOf("sync_scope_schema")))
+                null
+            else json.decodeFromString<SyncSet>(cursor.getText(columns.indexOf("sync_scope_schema"))),
+            setup = if (cursor.isNull(columns.indexOf("sync_scope_setup")))
+                null
+            else
+                json.decodeFromString<SyncSetup>(cursor.getText(columns.indexOf("sync_scope_setup"))),
+            version = if (cursor.isNull(columns.indexOf("sync_scope_version")))
+                SyncVersion.current
+            else
+                cursor.getText(columns.indexOf("sync_scope_version")),
+            lastSync = if (cursor.isNull(columns.indexOf("scope_last_sync")))
+                null
+            else
+                cursor.getLong(columns.indexOf("scope_last_sync")),
+            lastServerSyncTimestamp = if (cursor.isNull(columns.indexOf("scope_last_server_sync_timestamp")))
+                null
+            else
+                cursor.getLong(columns.indexOf("scope_last_server_sync_timestamp")),
+            lastSyncTimestamp = if (cursor.isNull(columns.indexOf("scope_last_sync_timestamp")))
+                null
+            else
+                cursor.getLong(columns.indexOf("scope_last_sync_timestamp")),
+            lastSyncDuration = if (cursor.isNull(columns.indexOf("scope_last_sync_duration")))
+                0
+            else
+                cursor.getLong(columns.indexOf("scope_last_sync_duration")),
             isNewScope = false
         )
+    }
 }
