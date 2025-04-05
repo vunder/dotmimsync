735d6a4e4140d89aba7406098565489773ec2a70
Aleksei Starchikov <vunder@bk.ru>
2025-04-05 21:01:13 +0200

    Setup* classes in KMP

--- dotmimsync-core/src/commonMain/kotlin/com/mimetis/dotmim/sync/setup/SetupColumns.kt
+++ dotmimsync-core/src/commonMain/kotlin/com/mimetis/dotmim/sync/setup/SetupColumns.kt
@@ -3,8 +3,54 @@ package com.mimetis.dotmim.sync.setup
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.serializer
 import com.mimetis.dotmim.sync.ArrayListLikeSerializer
+import com.mimetis.dotmim.sync.set.CustomList
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.listSerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.Decoder
 
 @Serializable(with = SetupColumnsSerializer::class)
-class SetupColumns : ArrayList<String>()
+class SetupColumns() : CustomList<String>() {
+    internal constructor(items: List<String>) : this() {
+        internalList.addAll(items)
+    }
+}
 
-object SetupColumnsSerializer : ArrayListLikeSerializer<SetupColumns, String>(String.serializer())
+@OptIn(ExperimentalSerializationApi::class)
+object SetupColumnsSerializer : ArrayListLikeSerializer<SetupColumns, String>(String.serializer()) {
+    override val descriptor: SerialDescriptor = listSerialDescriptor<String>()
+
+    override fun deserialize(decoder: Decoder): SetupColumns {
+        val items = ArrayList<String>()
+
+        val compositeDecoder = decoder.beginStructure(descriptor)
+
+        if (compositeDecoder.decodeSequentially()) {
+            val size = compositeDecoder.decodeCollectionSize(descriptor)
+            for (i in 0 until size) {
+                val element = compositeDecoder.decodeSerializableElement(
+                    elementSerializer.descriptor,
+                    i,
+                    elementSerializer
+                )
+                items.add(element)
+            }
+        } else {
+            while (true) {
+                val index =
+                    compositeDecoder.decodeElementIndex(elementSerializer.descriptor)
+                if (index == CompositeDecoder.DECODE_DONE) break
+                val element = compositeDecoder.decodeSerializableElement(
+                    elementSerializer.descriptor,
+                    index,
+                    elementSerializer
+                )
+                items.add(element)
+            }
+        }
+        compositeDecoder.endStructure(descriptor)
+
+        return SetupColumns(items)
+    }
+}
--- dotmimsync-core/src/commonMain/kotlin/com/mimetis/dotmim/sync/setup/SetupTables.kt
+++ dotmimsync-core/src/commonMain/kotlin/com/mimetis/dotmim/sync/setup/SetupTables.kt
@@ -1,13 +1,23 @@
 package com.mimetis.dotmim.sync.setup
 
-import kotlinx.serialization.Serializable
 import com.mimetis.dotmim.sync.ArrayListLikeSerializer
+import com.mimetis.dotmim.sync.set.CustomList
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.listSerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.Decoder
 
 /**
  * Represents a list of tables to be added to the sync process
  */
 @Serializable(with = SetupTablesSerializer::class)
-class SetupTables : ArrayList<SetupTable>() {
+class SetupTables() : CustomList<SetupTable>() {
+    internal constructor(items: List<SetupTable>) : this() {
+        internalList.addAll(items)
+    }
+
     /**
      * Get a table by its name
      */
@@ -25,4 +35,40 @@ class SetupTables : ArrayList<SetupTable>() {
     }
 }
 
-object SetupTablesSerializer : ArrayListLikeSerializer<SetupTables, SetupTable>(SetupTable.serializer())
+@OptIn(ExperimentalSerializationApi::class)
+object SetupTablesSerializer : ArrayListLikeSerializer<SetupTables, SetupTable>(SetupTable.serializer()) {
+    override val descriptor: SerialDescriptor = listSerialDescriptor<SetupTable>()
+
+    override fun deserialize(decoder: Decoder): SetupTables {
+        val items = ArrayList<SetupTable>()
+
+        val compositeDecoder = decoder.beginStructure(descriptor)
+
+        if (compositeDecoder.decodeSequentially()) {
+            val size = compositeDecoder.decodeCollectionSize(descriptor)
+            for (i in 0 until size) {
+                val element = compositeDecoder.decodeSerializableElement(
+                    elementSerializer.descriptor,
+                    i,
+                    elementSerializer
+                )
+                items.add(element)
+            }
+        } else {
+            while (true) {
+                val index =
+                    compositeDecoder.decodeElementIndex(elementSerializer.descriptor)
+                if (index == CompositeDecoder.DECODE_DONE) break
+                val element = compositeDecoder.decodeSerializableElement(
+                    elementSerializer.descriptor,
+                    index,
+                    elementSerializer
+                )
+                items.add(element)
+            }
+        }
+        compositeDecoder.endStructure(descriptor)
+
+        return SetupTables(items)
+    }
+}
