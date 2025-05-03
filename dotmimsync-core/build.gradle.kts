e80165b2f2e165f2084ff5773f7384b7acaac222
Aleksei Starchikov <vunder@bk.ru>
2025-05-03 16:05:42 +0200

    Version update

--- dotmimsync-core/build.gradle.kts
+++ dotmimsync-core/build.gradle.kts
@@ -96,7 +96,7 @@ publishing.publications
     .withType<MavenPublication>()
     .configureEach {
         groupId = "io.github.vunder.dotmimsync"
-        version = "1.1.0-kmp.alpha2"
+        version = "1.1.0-kmp.alpha3"
         if (versionSuffix.isNotBlank()) {
             version += "-$versionSuffix"
         }
