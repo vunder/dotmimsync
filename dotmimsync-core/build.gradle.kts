3ef6caf94d0ae5ea3f2bc5a6b172195b966e1e9c
Aleksei Starchikov <vunder@bk.ru>
2025-05-11 07:18:23 +0200

    Sqlite update

--- dotmimsync-core/build.gradle.kts
+++ dotmimsync-core/build.gradle.kts
@@ -96,7 +96,7 @@ publishing.publications
     .withType<MavenPublication>()
     .configureEach {
         groupId = "io.github.vunder.dotmimsync"
-        version = "1.1.0-kmp.alpha3"
+        version = "1.1.0-kmp.beta1"
         if (versionSuffix.isNotBlank()) {
             version += "-$versionSuffix"
         }
