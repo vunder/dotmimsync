3fd1e6056d50ac08c7da38323eb9396b21c3eebb
Aleksei Starchikov <vunder@bk.ru>
2025-06-16 16:35:45 +0200

    Updated ktor to 3.2.0

--- dotmimsync-core/build.gradle.kts
+++ dotmimsync-core/build.gradle.kts
@@ -85,7 +85,7 @@ publishing.publications
     .withType<MavenPublication>()
     .configureEach {
         groupId = "io.github.vunder.dotmimsync"
-        version = "1.1.1-beta3"
+        version = "1.1.1-beta4"
         if (versionSuffix.isNotBlank()) {
             version += "-$versionSuffix"
         }
