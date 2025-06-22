68028307140d0a26fa7f810eeb0b900335e24fd6
Aleksei Starchikov <vunder@bk.ru>
2025-06-22 21:27:29 +0200

    Upgrading sqlite, downgrading ktor due to error in it

--- dotmimsync-core/build.gradle.kts
+++ dotmimsync-core/build.gradle.kts
@@ -85,7 +85,7 @@ publishing.publications
     .withType<MavenPublication>()
     .configureEach {
         groupId = "io.github.vunder.dotmimsync"
-        version = "1.1.1-beta4"
+        version = "1.1.1-beta5"
         if (versionSuffix.isNotBlank()) {
             version += "-$versionSuffix"
         }
